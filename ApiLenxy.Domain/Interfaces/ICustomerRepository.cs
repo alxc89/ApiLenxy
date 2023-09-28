1df0ab2721deb3604c65a546a7132e1ec387fbbf
alxc89 <alexcaardoso@gmail.com>
2023-09-27 21:53:02 -0300

    Refatorando os repositorios

--- ApiLenxy.Domain/Interfaces/ICustomerRepository.cs
+++ ApiLenxy.Domain/Interfaces/ICustomerRepository.cs
@@ -1,9 +1,14 @@
 ﻿using ApiLenxy.Domain.Entites;
+using System.Linq.Expressions;
 
 namespace ApiLenxy.Domain.Interfaces;
 
-public interface ICustomerRepository : IRepositoryBase<Customer>
+public interface ICustomerRepository
 {
+    Task<Customer> GetById(Guid id);
+    Task<Customer> CreateAsync(Customer customer);
+    Task<Customer> UpdateAsync(Customer customer);
+    Task DeleteAsync(Guid id);
+    Task<IEnumerable<Customer>> GetAsync();
     Task<bool> VerifyExistsByDocumentAsync(string document);
-    Task<Customer> UpdateCustomer(Guid id, Customer customer);
 }
