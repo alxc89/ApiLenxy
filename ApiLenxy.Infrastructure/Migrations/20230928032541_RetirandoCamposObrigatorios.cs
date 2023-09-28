375ac2b75c979e18e5fc401d9ce6009aa55800e0
alxc89 <alexcaardoso@gmail.com>
2023-09-28 00:35:09 -0300

    Alteração na estrtura do banco, retirando obrigatóriedade de alguns campos

---
+++ ApiLenxy.Infrastructure/Migrations/20230928032541_RetirandoCamposObrigatorios.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiLenxy.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RetirandoCamposObrigatorios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Customer",
+                type: "VARCHAR(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Document",
+                table: "Customer",
+                type: "VARCHAR(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
